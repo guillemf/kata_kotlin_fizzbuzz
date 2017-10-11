47eded00bcabbc71fd4d9e646723bb9d33e472fd
Guillem Fernandez <hello@guillem.mobi>
2017-10-11 17:45:23 +0200

    First non basic feature passing

--- src/FizzBuzz/FizzBuzzTests.kt
+++ src/FizzBuzz/FizzBuzzTests.kt
@@ -7,9 +7,12 @@ import org.junit.Test
 class FizzBuzzTests {
     @Test fun fizzBuzz() {
         assertThat(fizzBuzzOf(1), `is`("1"))
+        assertThat(fizzBuzzOf(2), `is`("2"))
+        assertThat(fizzBuzzOf(3), `is`("Fizz"))
     }
 
     private fun  fizzBuzzOf(n: Int): String {
-        return "1"
+        if (n == 3) return "Fizz"
+        return n.toString()
     }
 }
