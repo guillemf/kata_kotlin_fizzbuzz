4f2a130b1b75d815b54c9122a9a8af51978ab891
Guillem Fernandez <hello@guillem.mobi>
2017-10-11 17:50:17 +0200

    Passing all basic tests

--- src/FizzBuzz/FizzBuzzTests.kt
+++ src/FizzBuzz/FizzBuzzTests.kt
@@ -9,10 +9,17 @@ class FizzBuzzTests {
         assertThat(fizzBuzzOf(1), `is`("1"))
         assertThat(fizzBuzzOf(2), `is`("2"))
         assertThat(fizzBuzzOf(3), `is`("Fizz"))
+        assertThat(fizzBuzzOf(4), `is`("4"))
+        assertThat(fizzBuzzOf(5), `is`("Buzz"))
+        assertThat(fizzBuzzOf(9), `is`("Fizz"))
+        assertThat(fizzBuzzOf(10), `is`("Buzz"))
     }
 
     private fun  fizzBuzzOf(n: Int): String {
-        if (n == 3) return "Fizz"
+
+        if (n % 3 == 0) return "Fizz"
+        if (n % 5 == 0) return "Buzz"
+
         return n.toString()
     }
 }
